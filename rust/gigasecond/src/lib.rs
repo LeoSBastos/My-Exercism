8f1e7746e76b2a65d42f578c575a1b2ec3da009d
Leonardo Bastos <leosbastos7@gmail.com>
2020-09-07 10:23:05 -0300

    Mentored Gigasecond Rust

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,6 @@
-use chrono::{DateTime, Utc};
-use chrono::Duration;
+use chrono::{DateTime, Duration, Utc};
+
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    let gigasecond: Duration = Duration::seconds(1_000_000_000);
-    start.checked_add_signed(gigasecond).unwrap()
+    start + Duration::seconds(1_000_000_000)'
 }

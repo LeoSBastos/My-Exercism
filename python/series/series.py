08257e1e84359e9f43f0bb3d0bba6dd404d2cfd4
Leonardo Bastos <leosbastos7@gmail.com>
2020-09-10 12:59:34 -0300

    Python 3

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,10 @@
 def slices(series, length):
-    pass
+    if length < 1 or len(series) < 1 or len(series) < length:
+        raise ValueError("Operation invalid.")
+    subS= []
+    if(length == len(series)):
+        subS.append(series)
+    else:
+        for i in range(0,len(series)-length+1):
+            subS.append(series[i:i+length])
+    return subS

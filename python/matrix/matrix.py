9121d94af41279bda0549816b90ea5848ea90d8c
Leonardo Bastos <leosbastos7@gmail.com>
2020-09-17 15:35:09 -0300

    Python 4

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,15 +1,8 @@
 class Matrix:
     def __init__(self, matrix_string):
-        rowsString = matrix_string.split("\n")
-        self.rows = [[] for x in rowsString]
-        for i in range(len(rowsString)):
-            self.rows[i] = [int(x) for x in rowsString[i].split(" ")]
-        self.columns =[[] for x in range(len(self.rows[0]))]
-        for r in self.rows:
-            for i in range(len(r)):
-                self.columns[i].append(r[i])
+        self.matrix = [[int(i) for i in row.split(' ')] for row in matrix_string.split('\n')]
     def row(self, index):
-        return self.rows[index-1]
+        return self.matrix[index-1]
 
     def column(self, index):
-        return self.columns[index-1]
+        return [r[index-1] for r in self.matrix]
